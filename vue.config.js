ac885783a5a88798d712ea39abfeb6c9b79f5af9
Noca <85557072+Nocae@users.noreply.github.com>
2021-06-27 15:14:52 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
+  transpileDependencies: ["vuetify"],
+
+  //vue解决跨域问题
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://www.baidu.com/",
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "/", //重写路径
+        },
+      },
+    },
+  },
+};
