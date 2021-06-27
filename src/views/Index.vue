08d8bb12c4cc77ba9e39eeaf86a0736cef722f49
LeoEchoAurora <leoechoaurora@gmail.com>
2021-06-27 16:54:49 +0800

    实现大概功能及样式

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,7 @@
 <!-- 组件说明 -->
 <template>
   <v-app>
-    <v-app-bar app height="36" class="white"> </v-app-bar>
+    <v-app-bar flat app height="36" class="white"> </v-app-bar>
 
     <v-main>
       <v-container fill-height>
@@ -14,23 +14,51 @@
                 src="https://www.baidu.com/img/540x258_2179d1243e6c5320a8dcbecd834a025d.png"
               ></v-img>
             </div>
-            <v-text-field
+            <div>
+              <div class="float-left pt-1" @click="logoShow = !logoShow">
+                <v-img
+                  src="@/assets/img/baidu.png"
+                  max-height="40"
+                  max-width="40"
+                  v-if="logoShow"
+                ></v-img>
+                <v-img
+                  src="@/assets/img/google.png"
+                  max-height="40"
+                  max-width="40"
+                  v-else
+                ></v-img>
+              </div>
+              <v-text-field
+                solo
+                label="请输入要搜索的内容"
+                class="rounded-pill pl-3"
+                append-icon="mdi-magnify"
+                v-model="searchKey"
+                @keyup.enter="baiduSearch"
+              ></v-text-field>
+            </div>
+
+            <!-- <v-text-field
               solo
               label="请输入要搜索的内容"
               class="rounded-pill"
-              prepend-inner-icon="mdi-google"
+              prepend-inner-icon="mdi-baidu"
               append-icon="mdi-magnify"
               v-model="searchKey"
               @keyup.enter="baiduSearch"
-            ></v-text-field>
+            ></v-text-field> -->
           </v-col>
         </v-row>
       </v-container>
     </v-main>
-    <v-footer app padless height="64" class="white">
+    <v-footer app padless height="64" class="white  text-body-2">
       <v-col class="text-center grey--text" cols="12">
         ©&nbsp;{{ new Date().getFullYear() }}&nbsp;&nbsp;.&nbsp;by&nbsp;
-        <strong>Noca</strong>
+        <a href="http://noca.top" target="_blank" rel="noopener noreferrer">
+          <strong>Noca</strong>
+        </a>
+        . All rights reserved.
       </v-col>
     </v-footer>
   </v-app>
@@ -44,6 +72,7 @@ export default {
   data() {
     return {
       searchKey: "",
+      logoShow: true,
     };
   },
   computed: {},
@@ -58,7 +87,12 @@ export default {
       //   .catch((err) => {});
       // console.log("11111");
       // this.$router.push(`/api/s?wd=${this.searchKey}`);
-      window.location.href = `http://www.baidu.com/s?ie=utf-8&wd=${this.searchKey}`;
+      if (!this.searchKey) return;
+      if (this.logoShow) {
+        window.location.href = `http://www.baidu.com/s?ie=utf-8&wd=${this.searchKey}`;
+      } else {
+        window.location.href = `https://www.google.com/search?q=${this.searchKey}&hl=zh-CN`;
+      }
     },
   },
 };
@@ -66,4 +100,8 @@ export default {
 
 <style scoped>
 /*@import url('')*/
+a {
+  text-decoration: none !important
+}
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,16 @@ module.exports = {
   transpileDependencies: ["vuetify"],
 
   //vue解决跨域问题
-  devServer: {
-    proxy: {
-      "/api": {
-        target: "http://www.baidu.com/",
-        ws: true,
-        changeOrigin: true,
-        pathRewrite: {
-          "^/api": "/", //重写路径
-        },
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://www.baidu.com/",
+  //       ws: true,
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         "^/api": "/", //重写路径
+  //       },
+  //     },
+  //   },
+  // },
 };
